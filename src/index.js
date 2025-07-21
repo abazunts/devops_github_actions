12d07cf12a25ab2b26f3a83c8d55fd8b22ccfd8a
abazunts <bazunc@gmail.com>
2025-07-21 11:14:57 +0400

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 4545;
  
-app.get('/api/ping', (req, res) => {
+app.get('/ping', (req, res) => {
   res.send({ message: 'pong' });
 });
  
